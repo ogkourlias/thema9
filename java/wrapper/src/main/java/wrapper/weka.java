cd497656e95df623c08fb7d2d34c66a7cb58ea7e
ogkourlias <orfeasgkourlias@gmail.com>
2022-11-05 16:58:39 +0100

    Changed the weka.runWeka function to properly read Arff and model data.

--- java/wrapper/src/main/java/wrapper/weka.java
+++ java/wrapper/src/main/java/wrapper/weka.java
@@ -1,32 +1,28 @@
 package wrapper;
 
-import weka.classifiers.trees.RandomForest;
+import weka.classifiers.meta.CostSensitiveClassifier;
 import weka.core.Instances;
-import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.SerializationHelper;
+import weka.core.converters.ArffLoader;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.util.Random;
+import java.io.*;
 
 public class weka {
 
-    static String train = "resources/train.arff";
-
     static void runWeka(String newFile) throws Exception {
-        Instances trainData = readArff(train);
+        Instances trainData = readArff("wrapper/build/resources/main/train.arff");
         Instances newData = readArff(newFile);
-        InputStream rForest_handle = new FileInputStream("resources/rForest.model");
-        RandomForest rf = (RandomForest) (new ObjectInputStream(rForest_handle)).readObject();
+        CostSensitiveClassifier rf = new CostSensitiveClassifier();
+        rf = (CostSensitiveClassifier) SerializationHelper.read("wrapper/src/main/resources/rforest.model");
 
     }
 
     private static Instances readArff(String handle) throws Exception {
-        DataSource source = new DataSource(handle);
-        Instances data = source.getDataSet();
-        if (data.classIndex() == -1)
-            data.setClassIndex(data.numAttributes() - 1);
+        BufferedReader reader =
+                new BufferedReader(new FileReader(handle));
+        ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
+        Instances data = arff.getData();
+        data.setClassIndex(data.numAttributes() - 1);
         return data;
     }
-
 }
